ebe0a974a17e7c7a2b1d49ba0a0023d74564e61f
DimaBukhal <dimabuhal@gmail.com>
2014-04-17 17:18:47 +0700

    alerts

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
     var wh = $(window).height();
-    var dh = $(document).height();
+    var dh = $(window).outerHeight;
     var dwh = wh - 90;
     $('.navbar-collapse').css('max-height', dwh + 'px');
     alert(wh,dh);
     $(window).resize(function() {
         var wh = $(window).height();
-         var dh = $(document).height();
+         var dh = $(window).outerHeight;
         var dwh = wh - 90;
         $('.navbar-collapse').css('max-height', dwh + 'px');
-         alert(wh,dh);
+          alert(wh,dh);
     });
 });
