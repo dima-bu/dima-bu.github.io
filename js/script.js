d8c372f4a8ae52fcab61b2cf377d71b9e29c5b8e
DimaBukhal <dimabuhal@gmail.com>
2014-04-25 19:45:51 +0700

    ajax test

--- js/script.js
+++ js/script.js
@@ -3,6 +3,18 @@ $(window).load(function() {
     var dwh = wh - 100;
     $('.navbar-collapse').css('max-height', dwh + 'px');
 
+    $('.navbar-bottom a').click(
+        function(){
+            $.ajax({
+                    url: 'match_details_lineups.html',
+                    success: function(){
+                        var attr1 = $(this).attr('id');
+                        alert(attr1, 'Load was performed.');
+                    }
+
+                });
+        });
+
     $(window).resize(function() {
         var wh = $(window).height();
         var dwh = wh - 90;
