426299aae4332968716ef25e90e9851a36768d70
DimaBukhal <dimabuhal@gmail.com>
2014-04-27 13:33:25 +0700

    almost all

--- js/script.js
+++ js/script.js
@@ -3,21 +3,26 @@ $(window).load(function() {
     var dwh = wh - 100;
     $('.navbar-collapse').css('max-height', dwh + 'px');
 
-    $('.navbar-bottom a').click(
-        function(){
-            $.ajax({
-                    url: 'match_details_lineups.html',
-                    success: function(){
-                        var attr1 = $(this).attr('id');
-                        alert(attr1, 'Load was performed.');
-                    }
 
-                });
-        });
 
     $(window).resize(function() {
         var wh = $(window).height();
         var dwh = wh - 90;
         $('.navbar-collapse').css('max-height', dwh + 'px');
     });
+
+    $('.navbar_calendar_link').on('click', function(){
+        $('.b_calendar').toggle();
+    });
+
+});
+
+$('.match-wrapper').on('click', '.navbar-bottom a',  function() {
+    var address=$(this).attr('id');
+    $('.match-wrapper').load("match_details_"+address+".html #match-"+address);
+});
+
+$('.b_calendar_wrapper').on('click', '.b_calendar_header a',  function() {
+    var address1=$(this).attr('id');
+    $('.b_calendar_wrapper').load("monthes.html .b_calendar#"+address1);
 });
