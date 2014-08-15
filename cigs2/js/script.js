8ba8db29151b6e8da88b8605978d03d1015f6bd5
dima-bu <dimabuhal@gmail.com>
2014-08-15 18:34:03 +0700

    add cigs2

---
+++ cigs2/js/script.js
@@ -0,0 +1,54 @@
+
+$(document).ready(function() {
+
+    $('.stars').raty({
+            starOff     : 'img/star-off.png',
+            starOn      : 'img/star-on.png'
+    });
+
+    $('.standart-select.-nicotine').ddslick({
+        width: 126
+    });
+
+    $('.standart-select-img.-flavour').ddslick(
+        {
+            width: 282
+    });
+
+    /*GALLERY*/
+
+    $(".product_img_gallery_item img").click(function() {
+        // see if same thumb is being clicked
+        if ($(this).parent('.product_img_gallery_item').hasClass("active")) { return; }
+
+        // calclulate large image's  based on the thumbnail URL (flickr specific)
+        var url = $(this).attr("alt");
+
+
+        var img = new Image();
+
+        // call this function after it's loaded
+        img.onload = function() {
+
+
+
+            // change the image
+            $("#product_img_big").attr("src", url);
+
+        };
+
+        // begin loading the image from www.flickr.com
+        img.src = url;
+
+        // activate item
+        $(".product_img_gallery_item").removeClass("active");
+        $(this).parent('.product_img_gallery_item').addClass("active");
+
+// when page loads simulate a "click" on the first image
+
+    }).filter(":first").click();
+
+    /*END GALLERY*/
+
+
+});
