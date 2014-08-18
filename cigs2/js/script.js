6e9bd0c2dcee5852019aa9f5c160d962879e4bb9
git <lenagim@mail.ru>
2014-08-18 16:44:29 +0700

    new blocks on HP (with some fix details in header and footer styles)

--- cigs2/js/script.js
+++ cigs2/js/script.js
@@ -15,11 +15,74 @@ $(document).ready(function() {
             width: 282
     });
 
+
+    $('.standart-select.-time').ddslick({
+        width: 96
+    });
+
+    $('.standart-select.-date-day').ddslick({
+        width: 130
+    });
+    $('.standart-select.-date-month').ddslick({
+        width: 130
+    });
+    $('.standart-select.-date-year').ddslick({
+        width: 130
+    });
+
+    $('.quantity-up').on( "click", function() {
+        $(this).parent('.quantity-input_wrapper').find('input').val( function(i, oldval) {
+            return ++oldval;
+        });
+    });
+
+    $('.quantity-drop').on( "click", function() {
+
+        $(this).parent('.quantity-input_wrapper').find('input').val( function(i, oldval) {
+            if (oldval > 0) {
+                return --oldval;
+            }
+
+            else {
+                return 0;
+            }
+        });
+    });
+
     /*GALLERY*/
+
+
+    $(".product_img_gallery .next-img").on( "click", function(e) {
+        e.preventDefault();
+        var wrap= $(this).parents('.product_img_gallery');
+//        wrap.addClass('red');
+
+        var index = wrap.find('.active');
+        var column = index.next('.column').index();
+//        console.log(column);
+        if (column > 0) {
+            index.next('.column').find('img').trigger('click');
+        }
+        else {
+            wrap.find('.column:first img').trigger('click');
+        }
+
+    });
+
+    $(".product_img_gallery .prev-img").on( "click", function(e) {
+        e.preventDefault();
+        var wrap= $(this).parents('.product_img_gallery');
+        var column = index.next('.column').index();
+        console.log(column);
+
+        var index = wrap.find('.active');
+        index.prev('.column').find('img').trigger('click');
+
+    });
 
     $(".product_img_gallery_item img").click(function() {
         // see if same thumb is being clicked
-        if ($(this).parent('.product_img_gallery_item').hasClass("active")) { return; }
+        if ($(this).parent('.product_img_gallery_item').parent('.column').hasClass("active")) { return; }
 
         // calclulate large image's  based on the thumbnail URL (flickr specific)
         var url = $(this).attr("alt");
@@ -41,8 +104,8 @@ $(document).ready(function() {
         img.src = url;
 
         // activate item
-        $(".product_img_gallery_item").removeClass("active");
-        $(this).parent('.product_img_gallery_item').addClass("active");
+        $(".column").removeClass("active");
+        $(this).parent('.product_img_gallery_item').parent('.column').addClass("active");
 
 // when page loads simulate a "click" on the first image
 
