37a856bcea880a6e16102179826a075a82456870
Dima <dimabuhal@gmail.com>
2015-12-16 17:39:18 +0600

    ...

--- any-vk/main.js
+++ any-vk/main.js
@@ -1,10 +1,14 @@
 angular.module('anyVk', ['ngStorage'])
-    .controller('mainCtrl', function ($scope, $http, vkGet, loginVK) {
+    .controller('mainCtrl', function ($scope, $http, vkGet, loginVK, loginVK2) {
 
         $scope.name = 'Дима';
         $scope.login = function (id, secret, url) {
             debugger;
-            loginVK(id, secret, url)
+            loginVK(id, secret, url);
+        };
+        $scope.login2 = function (id, secret, url) {
+            debugger;
+            loginVK2();
         };
 
         $scope.getName = function () {
@@ -41,7 +45,27 @@ angular.module('anyVk', ['ngStorage'])
             debugger;
             $http({
                 method: 'GET',
-                url: 'https://oauth.vk.com/access_token?client_id=' + id+'&client_secret='+secret+'&redirect_uri='+url
+                url: 'https://oauth.vk.com/access_token?client_id=?display=popup?response_type=token?' + id+'&client_secret='+secret+'&redirect_uri='+url
+            }).then(function successCallback (response) {
+                debugger;
+                deffer.resolve(response);
+            }, function errorCallback (response) {
+
+            });
+            return deffer.promise;
+
+        };
+
+        return loginVK;
+
+    }).factory('loginVK2', function ($http, $q, $localStorage) {
+        var deffer = $q.defer();
+
+        var loginVK = function () {
+            debugger;
+            $http({
+                method: 'GET',
+                url: 'https://oauth.vk.com/authorize?client_id=1&display=page&redirect_uri=http://http://dima-bu.github.io/any-vk/&scope=friends&response_type=token&v=5.41'
             }).then(function successCallback (response) {
                 debugger;
                 deffer.resolve(response);
