2821ed699de2cd658c03e0b0271c2ce9e293e878
Dima <dimabuhal@gmail.com>
2015-12-16 14:49:37 +0600

    ...

--- any-vk/main.js
+++ any-vk/main.js
@@ -1,18 +1,35 @@
 angular.module('anyVk', [])
-    .controller('mainCtrl', function($scope) {
+    .controller('mainCtrl', function($scope, $http, vkGet) {
 
-         $scope.name = '';
+         $scope.name = 'Дима';
          $scope.login = function () {
              VK.Auth.login();
          };
 
         $scope.getName = function () {
-            VK.Api.call('users.get', {}, function(r) {
-                if(r.response) {
-                    $scope.name = r.response[0].first_name;
-                }
-            });
+            debugger;
+            vkGet('users.get').then(
+                $scope.name = response.response[0].first_name;
+            )
         }
+    }).
+    factory('vkGet', function ($http, $q) {
+        var deffer = $q.defer();
+
+        var vkGet = function (method, token) {
+            $http({
+                method: 'GET',
+                url: 'https://api.vk.com/method/'+ method
+            }).then(function successCallback(response) {
+                deffer.resolve(response);
+            }, function errorCallback(response) {
+
+            });
+            return deffer.promise;
+        }
+
+        return vkGet;
+
     });
 
 
