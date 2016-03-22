2ab68882cf655f54c407d128d785cd6a6c2d43de
Dima Bukhal <dimabuhal@gmail.com>
2016-03-22 23:36:01 +0600

    ...

--- lendveri/code/js/script.js
+++ lendveri/code/js/script.js
@@ -2,4 +2,10 @@
 $(document).ready(function() {
     //debugger;
     //$('#carousel-example-generic').carousel();
+
+
+    $('.js-show-city-list').click(function () {
+        $('#city-list').toggle();
+    })
+    
 });
