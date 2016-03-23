aa60e3e910329b7ec3b6faa647cb1f395ed73d27
Dima Bukhal <dimabuhal@gmail.com>
2016-03-23 23:34:30 +0600

    ...

--- lendveri/code/js/script.js
+++ lendveri/code/js/script.js
@@ -1,11 +1,11 @@
 
 $(document).ready(function() {
-    //debugger;
-    //$('#carousel-example-generic').carousel();
-
 
     $('.js-show-city-list').click(function () {
         $('#city-list').toggle();
-    })
-    
+    });
+
+    $(".js-phone-mask").mask("+7(999) 999-99-99");
+
+
 });
