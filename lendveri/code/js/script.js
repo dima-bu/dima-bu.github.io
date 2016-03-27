0f0a8f93e8b7efe9c513cc4733f546e1b6128cf4
Dima Bukhal <dimabuhal@gmail.com>
2016-03-27 13:40:30 +0600

    ...

--- lendveri/code/js/script.js
+++ lendveri/code/js/script.js
@@ -7,5 +7,23 @@ $(document).ready(function() {
 
     $(".js-phone-mask").mask("+7(999) 999-99-99");
 
+    $('#gallery').simplegallery({
+        galltime : 400, // transition delay
+        gallcontent: '.gallery-content',
+        gallthumbnail: '.thumbnail',
+        gallthumb: '.thumb'
+    });
+
+    //var gallery = new $.ThumbnailGallery($('#gallery'), {
+    //    thumbImages: 'img/thumbs/thumb',
+    //    smallImages: 'img/large/image',
+    //    largeImages: 'img/large/image',
+    //    count: 3,
+    //    thumbImageType: 'jpg',
+    //    imageType: 'jpg',
+    //    breakpoint: 970,
+    //    shadowStrength: 1
+    //});
+
 
 });
