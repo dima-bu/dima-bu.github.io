03adeab37c5d662a4f952bd5053879548b9ccd8a
Dima Bukhal <dimabuhal@gmail.com>
2016-04-17 10:54:48 +0600

    починил свайп

--- lendveri/code/js/script.js
+++ lendveri/code/js/script.js
@@ -120,17 +120,27 @@ $(document).ready(function() {
 
 
     // добавление свайпа в карусель
-    $(".carousel").swipe( {
-        //Generic swipe handler for all directions
-        swipeLeft:function(event, direction, distance, duration, fingerCount) {
-            $(this).parent().carousel('prev');
-        },
-        swipeRight: function() {
-            $(this).parent().carousel('next');
-        },
-        //Default is 75px, set to 0 for demo so any distance triggers swipe
-        threshold:0
+    //$(".carousel").swipe( {
+    //    //Generic swipe handler for all directions
+    //    swipeLeft:function(event, direction, distance, duration, fingerCount) {
+    //        $(this).parent().carousel('prev');
+    //    },
+    //    swipeRight: function() {
+    //        $(this).parent().carousel('next');
+    //    },
+    //    //Default is 75px, set to 0 for demo so any distance triggers swipe
+    //    threshold:0
+    //});
+
+
+    $(".carousel").swiperight(function() {
+        $(this).parent().carousel('prev');
     });
+    $("#myCarousel").swipeleft(function() {
+        $(this).parent().carousel('next');
+    });
+
+
 
 
     $('.js-zoom').zoom({ touch: true});
