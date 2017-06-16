407605ba471d06fe1b88e0b02202adf646c1d1f9
Dima Bukhal <dimabuhal@gmail.com>
2017-06-16 22:15:04 +0600

    ...

--- sinertak/script.js
+++ sinertak/script.js
@@ -19,23 +19,80 @@ document.addEventListener('DOMContentLoaded', function(){
   bg2.css(initStyle);
 
   var isActiveBtn = true;
+  var currentIndex = '1';
 
-  paginationItem.click(function () {
+  if (window.addEventListener) {
+    if ('onwheel' in document) {
+      // IE9+, FF17+, Ch31+
+      window.addEventListener("wheel", onWheel);
+    } else if ('onmousewheel' in document) {
+      // устаревший вариант события
+      window.addEventListener("mousewheel", onWheel);
+    } else {
+      // Firefox < 17
+      window.addEventListener("MozMousePixelScroll", onWheel);
+    }
+  } else { // IE8-
+    window.attachEvent("onmousewheel", onWheel);
+  }
 
-    if ($(this).hasClass('active') || !isActiveBtn){
-      return false;
+
+  function onWheel(e) {
+    e = e || window.event;
+
+    //// wheelDelta не дает возможность узнать количество пикселей
+    var deltaX = e.deltaY || e.detail || e.wheelDelta;
+    //
+    //var info = document.getElementById('delta');
+    //
+    //info.innerHTML = +info.innerHTML + delta;
+
+    if (isActiveBtn) {
+
+      console.log('deltaX ', deltaX);
+      isActiveBtn = false;
+
+      paginationItem.removeClass('active');
+
+      if (currentIndex === '2') {
+        currentIndex = '1';
+        changeSlide('2');
+        $('.pagination-item[data-index=' + 1 + ']').addClass('active')
+      } else {
+        currentIndex = '2';
+        changeSlide('1');
+        $('.pagination-item[data-index=' + 2 + ']').addClass('active')
+      }
+
+      //if (deltaX > 0) {
+      //  changeSlide('2');
+      //} else {
+      //  changeSlide('1');
+      //}
+
     }
 
-    isActiveBtn = false;
-
-    var newIndex = $(this)[0].dataset.index;
-    var lastIndex = $('.pagination-item.active')[0].dataset.index;
+    //e.preventDefault ? e.preventDefault() : (e.returnValue = false);
+  }
 
 
-    paginationItem.removeClass('active');
-    $(this).addClass('active');
 
-    //background
+  function changeSlide(currentIndex){
+
+    console.log('changeSlide');
+
+    var newIndex;
+    var lastIndex = currentIndex;
+
+    if (lastIndex === '2') {
+      newIndex = '1'
+    } else {
+      newIndex = '2'
+    }
+
+    console.log(newIndex, lastIndex);
+
+    //paginationItem.removeClass('active');
 
     $('#img'+newIndex).css('z-index', 2);
     $('#img'+lastIndex).css('z-index', 1);
@@ -59,21 +116,6 @@ document.addEventListener('DOMContentLoaded', function(){
       ease: Power3.easeOut
     });
 
-    // TweenLite.to($('#bg'+newIndex), 0.5, {
-    //   css: {
-    //     transform: 'rotate(0deg)'
-    //   },
-    //   delay: 0.3,
-    //   ease: Power3.easeOut
-    // });
-
-    // TweenLite.to($('#bg'+newIndex), 0.4, {
-    //   css: {
-    //     transform: 'rotate(0deg)'
-    //   },
-    //   delay: 0.3
-    //   // ease: Power3.easeOut
-    // });
 
 
     TweenLite.to($('#bg'+lastIndex), 0, {
@@ -84,7 +126,7 @@ document.addEventListener('DOMContentLoaded', function(){
     //title
 
     TweenLite.to( $('#title'+lastIndex), 1, {
-      css: {transform: "translateY(40px)", opacity: 0},
+      css: {transform: "translateY(4rem)", opacity: 0},
       ease: Power3.easeOut,
       delay: 0.2
     });
@@ -96,14 +138,14 @@ document.addEventListener('DOMContentLoaded', function(){
     });
 
     TweenLite.to( $('#title'+lastIndex), 0, {
-      css: {transform: "translateX(-100px)"},
+      css: {transform: "translateX(-10rem)"},
       delay: 0.8
     });
 
     //subtitle
 
     TweenLite.to($('#subtitle'+lastIndex), 1, {
-      css: {transform: "translateY(40px)", opacity: 0},
+      css: {transform: "translateY(4rem)", opacity: 0},
       ease: Power3.easeOut
     });
 
@@ -121,12 +163,12 @@ document.addEventListener('DOMContentLoaded', function(){
     //descr
 
     TweenLite.to($('#descr'+lastIndex), 1, {
-      css: {'line-height': "40px", transform: "translateY(50px)", opacity: 0},
+      css: {'line-height': "4rem", transform: "translateY(50px)", opacity: 0},
       ease: Power3.easeOut
     });
 
     TweenLite.to($('#descr'+newIndex), 0.7, {
-      css: {'line-height': "30px", opacity: 1, transform: 'translateY(0px)'},
+      css: {'line-height': "3.4rem", opacity: 1, transform: 'translateY(0px)'},
       ease: Power3.easeOut,
       delay: 0.8
     });
@@ -142,8 +184,68 @@ document.addEventListener('DOMContentLoaded', function(){
     TweenLite.to($('#img'+lastIndex), 0.4, {
       css: {opacity: 0, transform: 'scale(0.8 ,0.8)'},
       ease: Power3.easeOut,
-      delay: 0.8
+      delay: 0.8,
+      onComplete: function () {
+        isActiveBtn = true;
+      }
     });
+
+  }
+
+
+  paginationItem.click(function () {
+
+
+    if (isActiveBtn) {
+      isActiveBtn = false;
+      paginationItem.removeClass('active');
+
+      if (currentIndex === '2') {
+        currentIndex = '1';
+        changeSlide('2');
+        $(this).addClass('active');
+      } else {
+        currentIndex = '2';
+        changeSlide('1');
+        $(this).addClass('active');
+      }
+    }
+
+
+
+    //if ($(this).hasClass('active') || !isActiveBtn){
+    //  return false;
+    //}
+    //
+    //isActiveBtn = false;
+    ////
+    //
+    //debugger;
+
+    //var newIndex = $(this)[0].dataset.index;
+    //var lastIndex = $('.pagination-item.active')[0].dataset.index;
+    //
+    //
+    //paginationItem.removeClass('active');
+    //$(this).addClass('active');
+
+    //changeSlide();
+
+
+    //background
+    //
+    //$('#img'+newIndex).css('z-index', 2);
+    //$('#img'+lastIndex).css('z-index', 1);
+    //$('#bg'+newIndex).css('z-index', 2);
+    //$('#bg'+lastIndex).css({'z-index': 1});
+    //$('#descr'+newIndex).css('z-index', 2);
+    //$('#descr'+lastIndex).css('z-index', 1);
+    //$('#title'+newIndex).css('z-index', 2);
+    //$('#title'+lastIndex).css('z-index', 1);
+    //$('#subtitle'+newIndex).css('z-index', 2);
+    //$('#subtitle'+lastIndex).css('z-index', 1);
+
+
 
 
     // TweenLite.to(sinTitle1, 1, {
