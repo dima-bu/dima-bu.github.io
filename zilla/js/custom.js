fbc9202e7f3bad09d6141de296cd40881d0b74d1
Dima Bukhal <dimabuhal@gmail.com>
2018-08-15 10:23:25 +0600

    form

--- zilla/js/custom.js
+++ zilla/js/custom.js
@@ -39,6 +39,17 @@
         });
     });
 
+        $(function() {
+        $('.contact-btn').bind('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 49
+            }, 1000);
+            event.preventDefault();
+        });
+    });
+
+
 
 
   /*-------------------------------------------------------------------------------
